a2499d0fc5f6d110a3ef0e472c609d5f144bcb15
TheBeff <thebeff@gmail.com>
2016-09-10 10:37:50 -0400

    Finished
    (before extra credit!)

--- browser/app.js
+++ browser/app.js
@@ -1,9 +1,23 @@
-window.whiteboard.on('draw', function(payload){
-	console.log(payload);
-});
-
 var socket = io(window.location.origin);
+var drawArray=[];
 
 socket.on('connect', function(){
 	console.log('I have made a persistent two-way connection to the server!');
+
+	whiteboard.on('draw', function(start, end, strokeColor){
+	  // drawArray.push({start, end, strokeColor, true});
+	  // console.log(drawArray);	
+	  socket.emit('draw', start, end, strokeColor);
+	});
+
+	socket.on('newDrawing', function(start, end, strokeColor){
+		// console.log(start, end, strokeColor);
+		whiteboard.draw(start, end, strokeColor, true);
+	});
+
+	socket.on('disconnect', function(){
+	   //console.log("NOOOOOOO");
+	   socket.emit('disconnect');
+    });
 });
+
--- server.js
+++ server.js
@@ -14,11 +14,21 @@ server.listen(1337, function () {
 });
 
 var io = socketio(server);
+
 io.on('connection', function (socket) {
     /* This function receives the newly connected socket.
        This function will be called for EACH browser that connects to our server. */
     console.log('A new client has connected!');
     console.log(socket.id);
+
+    socket.on('draw', function(start, end, strokeColor){
+    	//console.log(start, end, strokeColor);
+    	socket.broadcast.emit('newDrawing', start, end, strokeColor);
+    });
+
+    socket.on('disconnect', function(){
+	  console.log('A client has disconnected.'); 
+	});
 });
 
 app.use(express.static(path.join(__dirname, 'browser')));
